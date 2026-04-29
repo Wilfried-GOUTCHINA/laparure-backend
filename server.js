bdc68d7fa2c8b58f147e442933c129274c17566c
Wilfried-GOUTCHINA <wilfriedaderomou@gmail.com>
2026-04-29 22:50:36 +0200

    Fix CORS

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
